71852590c1a5b1396d7e4ee0677f43f8161f56ba
rpmartz <ryan@ryanpmartz.com>
2021-12-27 18:05:43 -0800

    day 1 kotlin

---
+++ src/main/kotlin/com/ryanpmartz/aoc/fifteen/Day01.kt
@@ -0,0 +1,34 @@
+package com.ryanpmartz.aoc.fifteen
+
+import java.io.File
+import java.nio.charset.Charset
+import kotlin.streams.asStream
+
+object Day01 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val instructions = File("src/main/resources/day01.txt").readText(Charset.defaultCharset())
+
+        var floor = 0
+        var enteredBasement = false
+        var count = 0
+        for (c in instructions.asSequence()) {
+            count += 1
+            if (c == ')') {
+                floor -= 1
+            }
+            else {
+                floor += 1
+            }
+
+            if(!enteredBasement && floor < 0) {
+                enteredBasement = true
+                println("Entered basement at step ${count}")
+            }
+        }
+
+        println(floor)
+
+    }
+}
