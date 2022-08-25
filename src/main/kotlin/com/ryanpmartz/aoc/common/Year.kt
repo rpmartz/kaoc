7da87a9af29f4f1b4cc2d343f1269f8c6edd662f
rpmartz <ryan@ryanpmartz.com>
2022-08-25 07:53:48 -0700

    add Year enum

---
+++ src/main/kotlin/com/ryanpmartz/aoc/common/Year.kt
@@ -0,0 +1,12 @@
+package com.ryanpmartz.aoc.common
+
+enum class Year(val value: String) {
+
+    FIFTEEN("2015"),
+    SIXTEEN("2016"),
+    SEVENTEEN("2017"),
+    EIGHTEEN("2018"),
+    NINETEEN("2019"),
+    TWENTY("2020"),
+    TWENTY_ONE("2021")
+}
