eec6d5d8d09453bbe6db1a163d0026c303588fdd
rpmartz <ryan@ryanpmartz.com>
2022-08-25 08:33:41 -0700

    input reader with ability to pass a transformation function

--- src/main/kotlin/com/ryanpmartz/aoc/common/InputReader.kt
+++ src/main/kotlin/com/ryanpmartz/aoc/common/InputReader.kt
@@ -5,8 +5,14 @@ import java.io.File
 object InputReader {
 
     private const val PATH_PREFIX = "src/main/resources"
+    private val IDENTITY: (line: String) -> String = { s: String -> s }
 
     fun read(year: AocYear, day: AocDayNumber): List<String> {
-        return File("$PATH_PREFIX/${year.value}/day${day.value}.txt").readLines()
+        return read(year, day, IDENTITY)
+    }
+
+    fun <T> read(year: AocYear, day: AocDayNumber, transformFn: (line: String) -> T): List<T> {
+        val lines = File("$PATH_PREFIX/${year.value}/day${day.value}.txt").readLines()
+        return lines.map { line -> transformFn(line) }
     }
 }
