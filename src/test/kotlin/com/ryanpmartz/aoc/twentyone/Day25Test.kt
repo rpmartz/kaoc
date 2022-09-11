5f88c7b400def42df7ceeb49c3368d8d36dec194
rpmartz <ryan@ryanpmartz.com>
2022-09-11 08:09:29 -0700

    test to expose bug

---
+++ src/test/kotlin/com/ryanpmartz/aoc/twentyone/Day25Test.kt
@@ -0,0 +1,32 @@
+package com.ryanpmartz.aoc.twentyone
+
+import com.ryanpmartz.aoc.common.Coordinate
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day25Test {
+
+    private val EXAMPLE_BOARD = """
+        v...>>.vv>
+        .vv>>.vv..
+        >>.>v>...v
+        >>v>>.>.v.
+        v>v.vv.v..
+        >.>>..v...
+        .vv..>.>v.
+        v.v..>>v.v
+        ....v..v.>
+    """.trimIndent()
+
+
+    @Test
+    fun testExample() {
+        val grid = Day25.parseGrid(EXAMPLE_BOARD.lines())
+
+        assertEquals(8, grid.maxX)
+        assertEquals(9, grid.maxY)
+        assertEquals(Cucumber(Direction.SOUTH), grid.state[Coordinate(0, 0)])
+        assertEquals(Cucumber(Direction.EAST), grid.state[Coordinate(8, 9)])
+        assertEquals(58, grid.runUntilStop())
+    }
+}
