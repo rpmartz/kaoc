0d591bfdec9318aaa216eebc3783d511e024a160
rpmartz <ryan@ryanpmartz.com>
2022-09-11 08:20:48 -0700

    day 24 input reading

---
+++ src/main/kotlin/com/ryanpmartz/aoc/twentyone/Day24.kt
@@ -0,0 +1,16 @@
+package com.ryanpmartz.aoc.twentyone
+
+import com.ryanpmartz.aoc.common.AocDayNumber
+import com.ryanpmartz.aoc.common.AocYear
+import com.ryanpmartz.aoc.common.io.InputReader
+
+object Day24 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = InputReader.read(AocYear.TWENTY_ONE, AocDayNumber.TWENTY_FOUR)
+        for (line in lines) {
+            println(line)
+        }
+    }
+}
