3b527ed31c1408dc745e9157bceaa638c9441ee6
rpmartz <ryan@ryanpmartz.com>
2023-01-02 11:45:37 -0800

    function to parse ints from text

---
+++ src/test/kotlin/com/ryanpmartz/aoc/common/ParsingTest.kt
@@ -0,0 +1,16 @@
+package com.ryanpmartz.aoc.common
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+class ParsingTest {
+
+    @Test
+    fun testParsingIntegersFromText() {
+        val text = "Sensor at x=3658528, y=641189: closest beacon is at x=4097969, y=-110334"
+        val actual = parseInts(text)
+        val expected = listOf(3658528, 641189, 4097969, -110334)
+
+        assertEquals(expected, actual)
+    }
+}
