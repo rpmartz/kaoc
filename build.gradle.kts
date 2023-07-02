bb24fa9b9e56d4ccbb66d75cda236d8de871b98e
rmartz <martzrp@gmail.com>
2023-07-01 22:27:46 -0700

    add junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.1"
-    id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
 }
 
-group = "com.example"
+group = "com.ryanpmartz.kaoc"
 version = "0.0.1-SNAPSHOT"
 
 java {
@@ -19,9 +16,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
 }
 
 tasks.withType<KotlinCompile> {
