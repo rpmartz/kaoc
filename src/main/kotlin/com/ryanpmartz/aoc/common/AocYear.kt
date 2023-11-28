004fad8d7e668db63d61240ea240f567d2a3ae1a
rmartz <martzrp@gmail.com>
2023-11-28 06:13:23 -0800

    add 2023

--- src/main/kotlin/com/ryanpmartz/aoc/common/AocYear.kt
+++ src/main/kotlin/com/ryanpmartz/aoc/common/AocYear.kt
@@ -9,5 +9,6 @@ enum class AocYear(val value: String) {
     NINETEEN("2019"),
     TWENTY("2020"),
     TWENTY_ONE("2021"),
-    TWENTY_TWO("2022")
+    TWENTY_TWO("2022"),
+    TWENTY_THREE("2023")
 }
