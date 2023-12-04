b10ed53addfacce348e9391efb73c8d240252dd9
rmartz <martzrp@gmail.com>
2023-12-03 21:28:20 -0800

    day 4 part 1

---
+++ src/main/kotlin/com/ryanpmartz/aoc/y23/Day04.kt
@@ -0,0 +1,59 @@
+package com.ryanpmartz.aoc.y23
+
+import com.ryanpmartz.aoc.common.AocDayNumber
+import com.ryanpmartz.aoc.common.AocYear
+import com.ryanpmartz.aoc.common.io.InputReader
+import java.lang.Integer.max
+
+data class ScratchoffCard(val winningNumbers: Set<Int>, val cardNumbers: List<Int>) {
+
+    fun calculateScore(): Int {
+        var numMatches = 0
+        for (cardNumber in cardNumbers) {
+            if (winningNumbers.contains(cardNumber)) {
+                numMatches = max(1, numMatches * 2)
+            }
+        }
+
+        return numMatches
+    }
+}
+
+object Day04 {
+
+    fun parseSpaceDelimitedNumbers(s: String): List<Int> {
+        val textNumbers = s.trim().split(" ")
+
+
+
+        return textNumbers.filter { it.isNotEmpty() }.map { it.toInt() }
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = InputReader.read(AocYear.TWENTY_THREE, AocDayNumber.FOUR)
+
+        val cards = mutableListOf<ScratchoffCard>()
+
+        for (line in lines) {
+            val numbers = line.substringAfter(":").trim()
+            val components = numbers.split("|")
+
+            val winningNumbers = parseSpaceDelimitedNumbers(components[0]).toSet()
+            val cardNumbers = parseSpaceDelimitedNumbers(components[1])
+
+            val card = ScratchoffCard(winningNumbers, cardNumbers)
+
+            cards.add(card)
+
+        }
+
+        var total = 0
+        for (card in cards) {
+            total += card.calculateScore()
+        }
+
+        println(total)
+
+    }
+}
