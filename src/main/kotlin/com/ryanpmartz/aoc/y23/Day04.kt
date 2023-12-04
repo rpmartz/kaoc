92ec15958d3a6c86bf7b8f89eff082997bff8059
rmartz <martzrp@gmail.com>
2023-12-03 21:38:21 -0800

    extract method

--- src/main/kotlin/com/ryanpmartz/aoc/y23/Day04.kt
+++ src/main/kotlin/com/ryanpmartz/aoc/y23/Day04.kt
@@ -8,10 +8,21 @@ import java.lang.Integer.max
 data class ScratchoffCard(val winningNumbers: Set<Int>, val cardNumbers: List<Int>) {
 
     fun calculateScore(): Int {
+        var score = 0
+        for (cardNumber in cardNumbers) {
+            if (winningNumbers.contains(cardNumber)) {
+                score = max(1, score * 2)
+            }
+        }
+
+        return score
+    }
+
+    fun numMatches(): Int {
         var numMatches = 0
         for (cardNumber in cardNumbers) {
             if (winningNumbers.contains(cardNumber)) {
-                numMatches = max(1, numMatches * 2)
+                numMatches++
             }
         }
 
@@ -29,21 +40,20 @@ object Day04 {
         return textNumbers.filter { it.isNotEmpty() }.map { it.toInt() }
     }
 
+    private fun parseLine(l: String): ScratchoffCard {
+        val numbers = l.substringAfter(":").trim()
+        val components = numbers.split("|")
+
+        val winningNumbers = parseSpaceDelimitedNumbers(components[0]).toSet()
+        val cardNumbers = parseSpaceDelimitedNumbers(components[1])
+
+        val card = ScratchoffCard(winningNumbers, cardNumbers)
+
+        return card
+    }
+
     private fun partOne(lines: List<String>) {
-        val cards = mutableListOf<ScratchoffCard>()
-
-        for (line in lines) {
-            val numbers = line.substringAfter(":").trim()
-            val components = numbers.split("|")
-
-            val winningNumbers = parseSpaceDelimitedNumbers(components[0]).toSet()
-            val cardNumbers = parseSpaceDelimitedNumbers(components[1])
-
-            val card = ScratchoffCard(winningNumbers, cardNumbers)
-
-            cards.add(card)
-
-        }
+        val cards = lines.map { parseLine(it) }
 
         var total = 0
         for (card in cards) {
